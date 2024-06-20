4608cd0ce60b0745b39e2c9967273c178b4c202f
Marcin Jagieła <marcin.jagiela@hotmail.com>
2024-06-20 19:06:45 +0200

    Memento pattern done

---
+++ ExampleDesignPatterns/MementoPattern/Document.cs
@@ -0,0 +1,65 @@
+﻿namespace MementoPattern;
+
+public class Word
+{
+    public readonly Document _document;
+    public readonly History _history;
+
+    public Word(Document document, History history)
+    {
+        _document = document;
+        _history = history;
+    }
+
+    public void SetContent(string content) 
+    {
+        _history.Save(new DocumentMemento(_document.GetContent()));
+        _document.SetContent(content);
+    }
+
+    public string GetContent() => _document.GetContent();
+
+    public void RestoreContent() => _document.SetContent(_history.Retrieve().Content);
+}
+
+public class Document
+{
+    private string _content;
+
+    public string GetContent() => _content;
+
+    public string SetContent(string content) => _content = content;
+}
+
+public class DocumentMemento
+{
+    public string Content { get; }
+
+    public DocumentMemento(string content) => Content = content;
+}
+
+public class History 
+{
+    private readonly Stack<DocumentMemento> _history = new();
+
+    public void Save(DocumentMemento memento) => _history.Push(memento);
+
+    public DocumentMemento Retrieve() => _history.Pop();
+}
+
+//Refactor this because it violates SRP
+//public class Document
+//{
+//    private string _content;
+
+//    private readonly Stack<string> _history = new Stack<string>();
+
+//    public string GetContent() => _content;
+
+//    public string SetContent(string content) => _content = content;
+
+
+//    public void CreateState() => _history.Push(_content);
+
+//    public void RestoreState() => _content = _history.Pop();
+//}
---
+++ ExampleDesignPatterns/MementoPatternTests/DocumentTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using MementoPattern;
+
+namespace MementoPatternTests;
+
+public class DocumentTests
+{
+    [Fact]
+    public void Document_works()
+    {
+        var document = new Document();
+
+        document.SetContent("Some stuff");
+        document.GetContent().Should().Be("Some stuff");
+
+        document.CreateState();
+        document.SetContent("Something not good");
+        document.GetContent().Should().Be("Something not good");
+
+        document.RestoreState();
+        document.GetContent().Should().Be("Some stuff");
+    }
+}
